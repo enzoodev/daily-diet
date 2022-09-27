f9c86816d6c989cd932ac91fc024d712d42b9d62
Enzo Damascena <ninodm.dev@gmail.com>
2022-09-27 09:17:05 -0300

    Update styles.ts

--- src/components/ListEmpty/styles.ts
+++ src/components/ListEmpty/styles.ts
@@ -2,13 +2,15 @@ import styled, { css } from "styled-components/native";
 
 const Container = styled.View`
     flex: 1;
+    justify-content: center;
+    align-items: center;
 `
 
 const Title = styled.Text`
     ${({ theme }) => css`
-        font-family: ;
-        font-size: ;
-        color: ;
+        font-family: ${theme.FONTS.FAMILY.REGULAR};
+        font-size: ${theme.FONTS.SIZE.MD};
+        color: ${theme.COLORS.BASE.GRAY_3};
     `};
 `
 
