8a523a97b3fc73d80260b635544a934022fed4d0
Enzo Damascena <ninodm.dev@gmail.com>
2022-10-05 12:35:46 -0300

    Create index.tsx

---
+++ src/components/DietInfo/index.tsx
@@ -0,0 +1,17 @@
+import { TouchableOpacityProps } from 'react-native';
+import * as S from './styles';
+
+type Props = TouchableOpacityProps & S.DietInfoTypeStyleProps;
+
+const DietInfo = ({ IsInDiet, ...rest }: Props) => {
+    return(
+        <S.Container {...rest}>
+            <S.Icon isInDiet={isInDiet} />
+            <S.Title>
+                { isInDiet ? 'dentro' : 'fora' } da dieta
+            </S.Title>
+        </S.Container>
+    )
+}
+
+export default DietInfo;
