0ed15e498aefb9e8298947028c052cb2c0d2665b
Enzo Damascena <ninodm.dev@gmail.com>
2022-10-05 11:15:22 -0300

    Update styles.ts

--- src/components/DietInfo/styles.ts
+++ src/components/DietInfo/styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from styled-components/native';
+import styled, { css } from "styled-components/native";
 
 type DietInfoTypeStyleProps = {
     type: 'PRIMARY' | 'SECONDARY';
