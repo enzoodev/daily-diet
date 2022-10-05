f64d22c5a26c947c2a22065cf6b521160a984703
Enzo Damascena <ninodm.dev@gmail.com>
2022-10-05 11:14:27 -0300

    create components/DietInfo/styles.ts

---
+++ src/components/DietInfo/styles.ts
@@ -0,0 +1,34 @@
+import styled, { css } styled-components/native';
+
+type DietInfoTypeStyleProps = {
+    type: 'PRIMARY' | 'SECONDARY';
+}
+
+type Props = DietInfoTypeStyleProps;
+
+const Container = styled.TouchableOpacity`
+    padding: 18px 24px;
+    border-radius: 20px;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    background-color: ${({ theme }) => theme.COLORS.BASE.GRAY_6};
+`
+
+const Icon = styled.View<Props>`
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    background-color: ${({ theme, type }) => type === 'PRIMARY' ?
+    theme.COLORS.PRODUCT.GREEN_DARK : theme.COLORS.PRODUCT.RED_DARK};
+`
+
+comst Title = styled.Text`
+    ${({ theme }) => css`
+        font-family: ${theme.FONTS.FAMILY.REGULAR};
+        font-size: ${theme.FONTS.SIZE.SM}px;
+        color: ${theme.COLORS.BASE.GRAY_1};
+    `};
+`
+
+export { DietInfoTypeStyleProps, Container, Icon, Title };
