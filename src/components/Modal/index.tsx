6198bc1331c25fa0ac6f80d9f707d087c9c926e3
Enzo Damascena <ninodm.dev@gmail.com>
2022-10-06 14:03:09 -0300

    Update index.tsx

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,14 +1,44 @@
+import { TouchableOpacityProps } from 'react-native';
 import * as S from './styles';
 
-type Props = {
-
+type Props = TouchableOpacityProps & S.ModalTypeStyleProps & {
+    isVisible: boolean;
+    title: string;
+    buttonTitle: string;
+    buttonTitleTwo?: string;
 }
 
-const Modal = ({  }: Props) => {
+const Modal = ({ isVisible, title, type, ...rest }: Props) => {
     return(
-        <S.Container>
-
-        </S.Container>
+        { isVisible &&
+            <S.Container>
+                <S.Content>
+                    <S.Title>
+                        {title}
+                    </S.Title>
+                    <S.ContentButton>
+                        <S.Button
+                            type={type}
+                            {...rest}
+                        >
+                            <S.ButtonTitle>
+                                {buttonTitle}
+                            </S.ButtonTitle>
+                        </S.Button>
+                        { buttonTitleTwo &&
+                            <S.Button
+                                type={type}
+                                {...rest}
+                            >
+                                <S.ButtonTitle>
+                                    {buttonTitleTwo}
+                                </S.ButtonTitle>
+                            </S.Button>
+                        }
+                    </S.ContentButton>
+                </S.Content>  
+            </S.Container>
+        }
     )
 }
 
